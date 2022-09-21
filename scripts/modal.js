10ad6a4161edc4175299e91755188d5b8eb13381
lucasbatista-dev <lucas_pivatto@hotmail.com.br>
2022-09-21 19:48:13 -0300

    entrega finalizada

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,1 +1,10 @@
-/* Desenvolva seu código aqui... */
+const buttonsControllersModal = document.querySelectorAll(
+  "[data-control-modal]"
+);
+for (let i = 0; i < buttonsControllersModal.length; i++) {
+  buttonsControllersModal[i].addEventListener("click", () => {
+    let modalId = buttonsControllersModal[i].getAttribute("data-control-modal");
+    console.log(modalId);
+    document.getElementById(modalId).classList.toggle("show__modal");
+  });
+}
